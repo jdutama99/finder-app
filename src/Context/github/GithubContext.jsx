726a07e8f7c5b6c6550d787a7c7a0070c8c97c25
Jason <jasonutama0206@gmail.com>
2022-08-11 15:22:55 -0400

    second commit

--- src/Context/github/GithubContext.jsx
+++ src/Context/github/GithubContext.jsx
@@ -14,24 +14,27 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducers, initialState)
 
-  //Get initial users (testing purposes)
-  const fetchUsers = async () => {
+  //Get search results
+  const searchUsers = async (text) => {
     setLoading()
 
-    const response = await fetch(`${GITHUB_URL}/users`, {
+    const params = new URLSearchParams({ q: text })
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     })
 
-    const data = await response.json()
+    const { items } = await response.json()
 
     dispatch({
       type: 'GET_USERS',
-      payload: data,
+      payload: items,
     })
   }
-
+  // clear user from state
+  const clearUsers = () => dispatch({ type: 'CLEAR_USERS' })
   //set Loading
   const setLoading = () => dispatch({ type: 'SET_LOADING' })
   return (
@@ -39,7 +42,8 @@ export const GithubProvider = ({ children }) => {
       value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers,
+        searchUsers,
+        clearUsers,
       }}
     >
       {children}
