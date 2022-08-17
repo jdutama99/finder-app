6172d5f047d8d1ffe5f717421408697e1ed3606f
Jason <jasonutama0206@gmail.com>
2022-08-17 17:25:31 -0400

    ver-1 commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './Pages/Home'
 import NotFound from './Pages/NotFound'
+import Home from './Pages/Home'
 import About from './Pages/About'
+import User from './Pages/User'
 import NavBar from './components/layout/NavBar'
 import Footer from './components/layout/Footer'
+import Alert from './components/layout/Alert'
 import { GithubProvider } from './Context/github/GithubContext'
+import { AlertProvider } from './Context/alert/AlertContext'
 
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <NavBar />
-          <main className="container mx-auto px-3 pb-12">
-            <Routes>
-              <Route path="/home" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/notfound" element={<NotFound />} />
-              <Route path="/*" element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <NavBar />
+            <main className="container mx-auto px-3 pb-12">
+              <Alert />
+              <Routes>
+                <Route path="/home" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
+                <Route path="/notfound" element={<NotFound />} />
+                <Route path="/*" element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   )
 }
--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,17 +1,19 @@
 import { useState, useContext } from 'react'
 import GithubContext from '../../Context/github/GithubContext'
+import AlertContext from '../../Context/alert/AlertContext'
 
 function UserSearch() {
   const [text, setText] = useState('')
 
   const { users, searchUsers, clearUsers } = useContext(GithubContext)
+  const { setAlert } = useContext(AlertContext)
   const handleChange = (e) => setText(e.target.value)
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
     if (text === '') {
-      alert('please enter something')
+      setAlert('please enter something', 'error')
     } else {
       searchUsers(text)
       setText('')
@@ -26,7 +28,7 @@ function UserSearch() {
             <div className="relative">
               <input
                 type="text"
-                className="w-fll pr-40 bg-gray-200 input input-lg text-black"
+                className="w-full pr-40 bg-gray-200 input input-lg text-black"
                 placeholder="Search"
                 value={text}
                 onChange={handleChange}
